02cb7b3300d26514e8342f7239056c6ffc6b76a8
novakvova <novakvova@gmail.com>
2022-09-11 11:09:46 +0300

    working seed data user and sycurity begin

--- api.kids/src/main/java/org/example/configuration/security/SecurityConfig.java
+++ api.kids/src/main/java/org/example/configuration/security/SecurityConfig.java
@@ -1,13 +1,83 @@
 package org.example.configuration.security;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//
-//@Configuration
-//@EnableGlobalMethodSecurity(
-//        securedEnabled = true,
-//        jsr250Enabled = true,
-//        prePostEnabled = true
-//)
-public class SecurityConfig  {
+import org.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${springdoc.api-docs.path}")
+    private String restApiDocPath;
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerPath;
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .userDetailsService(userService) //Як перевіряється користувач, який авторизовано
+                .passwordEncoder(encoder); //Еncoder, який шифрує пароль
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Enable CORS and disable CSRF
+        http = http.cors().and().csrf().disable();
+
+        // Set session management to stateless
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Set unauthorized requests exception handler
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            //logger.error("Unauthorized request - {}", ex.getMessage());
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                        }
+                )
+                .and();
+
+        // Set permissions on endpoints
+        http.authorizeRequests()
+                // Swagger endpoints must be publicly accessible
+                .antMatchers("/**").permitAll()
+                .antMatchers("/static/**").permitAll() //.hasAuthority(Roles.Admin)
+                .antMatchers("/api/public/**").permitAll()
+                .antMatchers(String.format("%s/**", restApiDocPath)).permitAll()
+                .antMatchers(String.format("%s/**", swaggerPath)).permitAll()
+                .anyRequest().authenticated();
+
+    }
+    // Expose authentication manager bean
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
 }
--- api.kids/src/main/java/org/example/loader/DatabaseLoader.java
+++ api.kids/src/main/java/org/example/loader/DatabaseLoader.java
@@ -2,11 +2,17 @@ package org.example.loader;
 
 import org.example.constants.Roles;
 import org.example.entities.RoleEntity;
+import org.example.entities.UserEntity;
 import org.example.repositories.RoleRepository;
 import org.example.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
@@ -25,6 +31,17 @@ public class DatabaseLoader implements CommandLineRunner {
         if(this.roleRepository.count() == 0) {
             this.roleRepository.save(new RoleEntity(Roles.Admin));
             this.roleRepository.save(new RoleEntity(Roles.User));
+        }
+        if(this.userRepository.count()==0)
+        {
+            PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            UserEntity user = new UserEntity();
+            user.setEmail("admin@gmail.com");
+            user.setName("Сашко Юхимович");
+            user.setPhone("096 620 73 778");
+            user.setPassword(encoder.encode("123456"));
+            user.setRoles(Arrays.asList(roleRepository.findByName(Roles.Admin)));
+            this.userRepository.save(user);
         }
     }
 }
